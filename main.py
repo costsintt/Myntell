8d9c36d266e5407384c3a00c95b91ecc199cce1d
costsitt <Eg.Helik25@gmail.com>
2023-06-12 14:56:00 +0300

    refactor: align with changes in the apis, improve readability

--- main.py
+++ main.py
@@ -1,98 +1,121 @@
-from speechTools import *
-from info import *
+# f'Check the transcribed audio input for speech errors, including\
+#   incorrect sentence formation, missing or incorrect prepositions, and any\
+#   other speech and logical errors. If the sentence is correct, output "Correct."\
+#   If the sentence is incorrect, provide an explanation\
+#   for each problem found. The message: "{message}".'
 
 import sounddevice as sd
 import soundfile as sf
-
-import time
-import keyboard
 import numpy as np
-import math
-import boto3
-import requests
-import openai
-import chatbot
-import os
-import shutil
 
+from io import BytesIO
+from typing import Union, BinaryIO
+
+import pygame
+
+from chatAIAPIClient_openai import ChatAIAPIClient
+from speechToTextAPIClient_openai import SpeechToTextAPIClient
+from textToSpeechAPIClient_gtts import TextToSpeechAPIClient
+from chatbot import Chatbot
+
+
+class AudioRecorder:
+    def __init__(self, samplerate=44100):
+        self.isRecording = False
+        self.samplerate = samplerate
+        self.buffer = []
+        self.stream = sd.InputStream(callback=self.callback, channels=1, samplerate=samplerate)
+
+    def callback(self, indata, frames, time, status):
+        self.buffer.append(indata.copy())
+
+    def start_recording(self):
+        if not self.isRecording:
+            self.isRecording = True
+            self.stream.start()
+
+    def stop_recording(self):
+        if self.isRecording:
+            self.isRecording = False
+            self.stream.stop()
+            recording = np.concatenate(self.buffer, axis=0)
+            self.buffer = []
+            return recording
+
+def file_read(filename: str) -> Union[BytesIO, None]:
+    try:
+        with open(filename, 'rb') as file:
+            content = file.read()
+    except FileNotFoundError:
+        return None
+    else:
+        clone = BytesIO(content)
+        return clone
+
+def file_write(fileToWrite: BytesIO, filename: str) -> None:
+    with open(filename, 'wb') as file:
+        file.write(fileToWrite.getbuffer())
 
 if __name__ == "__main__":
+    pygame.init()
+    pygame.mixer.init()
+    from apiKeys import apiKey_openai
+
+    sttAPI = SpeechToTextAPIClient(api_key=apiKey_openai)
+    ttsAPI = TextToSpeechAPIClient()
+    chatAPI = ChatAIAPIClient(api_key=apiKey_openai)
     
-    listForSavingConversationInText = []
+    grammarBot = Chatbot(chatAPI,
+                         behaviour="Correct grammar and assist non-English speakers in expressing \
+                            themselves in English. If the input text is not in English, translate \
+                            it into English.")
+    conversationalBot = Chatbot(chatAPI)
 
-    fileName_synthesized = 'output.mp3'
-    fileName_micRecorded = 'outoutMicFile.mp3'
-    fileName_micRecordedToText = 'micOutPut.txt'
+    recorder = AudioRecorder()
 
-    recordedAudioFromMic = None
-    sampleRate = 44100
-    numOfChannels = 2
-    maxDuration = 30
-    isMicRecording = False
-
-    timeOfStartedAudioRecordingFromMic = 0.0
-
-    botik = chatbot.Chatbot()
-
+    isRecording = False
     while(True):
-        if not isMicRecording and keyboard.is_pressed('shift+alt+1'):
-            print("[Mic recording: STARTED]")
-            isMicRecording = True
-            timeOfStartedAudioRecordingFromMic = time.time()
-            recordedAudioFromMic = sd.rec(int(sampleRate * maxDuration),
-                                          samplerate = sampleRate, channels = numOfChannels)
+        command = input()
+        if command == '1':
+            print("Recording started...", sep='')
+            isRecording = True
+            recorder.start_recording()
+        elif (command == '2' or command == '3') and isRecording:
+            print("STOPPED")
+            isRecording = False
+            data = recorder.stop_recording()
+            audioFile = BytesIO()
+            sf.write(audioFile, data, 44100, format="wav")
+            audioFile.seek(0)
             
-        if isMicRecording and keyboard.is_pressed('shift+alt+2'):
-            print("[Mic recording: STOPPED]")
-            isMicRecording = False
-            sd.stop()
-            elapsedTime = recordedAudioFromMic[
-                :int( math.ceil(time.time() - timeOfStartedAudioRecordingFromMic) * sampleRate )
-                ]
-            sf.write(fileName_micRecorded, elapsedTime, samplerate = sampleRate, format = 'mp3')
-            recordedAudioFromMic = None
-
-            current_dir = os.path.abspath(os.path.dirname(__file__))
-            file_path = os.path.join(current_dir, fileName_micRecorded)
-            dest_dir = os.path.join(current_dir, 'myAudio')
-            if not os.path.exists(dest_dir):
-                os.makedirs(dest_dir)
-            shutil.copy(file_path, dest_dir)
-            os.rename(os.path.join(dest_dir, fileName_micRecorded), os.path.join(dest_dir, str(time.time()) + '.mp3'))
-
-            convertAudioToText(fileName_micRecorded, fileName_micRecordedToText)
+            print("---GRAMMAR---") if command == '2' else print("---CONVERSATION---")
+            userMessage = sttAPI.transcribe(audioFile)
+            print("User:\t", userMessage)
+            botMessage = grammarBot.chat(userMessage) if command == '2' else conversationalBot.chat(userMessage)
+            print("Bot:\t", botMessage)
             
-            text_file = open(fileName_micRecordedToText, "r")
-            micRecordedText = text_file.read()
-            text_file.close()
+            audioFile.seek(0)
+            audioFile.truncate(0)
+            audioFile.write(ttsAPI.synthesize(botMessage))
 
-            print("User: " + micRecordedText)
-            resultOfCheckingGrammar = botik.checkGrammar(micRecordedText)
-            if len(resultOfCheckingGrammar) > 10:
-                print("AI-corrector: " + resultOfCheckingGrammar)
-                synthesizeAudioFromText(resultOfCheckingGrammar, fileName_synthesized)
-                playAudio(fileName_synthesized)
+            audioFile.seek(0)
+            file_write(audioFile, "temp.mp3")
+            pygame.mixer.music.load("temp.mp3")
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
+        elif command == '4' or command == '5':
+            print("---GRAMMAR---") if command == '4' else print("---CONVERSATION---")
+            userMessage = input('write input: ')
+            botMessage = grammarBot.chat(userMessage) if command == '4' else conversationalBot.chat(userMessage)
+            print("Bot:\t", botMessage)
 
-                listForSavingConversationInText.append("User: " + micRecordedText)
-                listForSavingConversationInText.append("AI-corrector: " + resultOfCheckingGrammar)
-            else:
-                aiAnswer = botik.chat(micRecordedText)
-                print("AI: " + aiAnswer)
-                synthesizeAudioFromText(aiAnswer, fileName_synthesized)
-                playAudio(fileName_synthesized)
 
-                listForSavingConversationInText.append("User: " + micRecordedText)
-                listForSavingConversationInText.append("AI: " + aiAnswer)
-        
-        if isMicRecording and keyboard.is_pressed('shift+alt+3'):
-            print("[Mic recording: INTERRUPTED]")
-            isMicRecording = False
-            sd.stop()
-            recordedAudioFromMic = None
-
-        if not isMicRecording and keyboard.is_pressed('shift+alt+4'):
-            print("[EXIT]")
-            with open(f'{time.time()}.txt', "w") as f:
-                for string in listForSavingConversationInText:
-                    f.write(string + "\n")
-            exit()
+        elif command == '0':
+            print("ALL MESSAGES")
+            print("---conversational bot---")
+            for message in conversationalBot.messages:
+                print(message)
+            print("---grammar bot---")
+            for message in conversationalBot.messages:
+                print(message)
